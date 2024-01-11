ec8573265c215e234d6b02ddd6c0931a3c7fc188
suganeshflavio <eshwarsuganeshwaran5@gmail.com>
2024-01-11 11:03:47 +0530

    latest code after deleting admin route stuff

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,9 @@ import { useRoutes } from 'react-router-dom';
 // routes
 import MainRoutes from './MainRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
-import AdminRoute from './AdminRoute';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-  return useRoutes([ AuthenticationRoutes,MainRoutes,AdminRoute]);
+  return useRoutes([ AuthenticationRoutes,MainRoutes]);
 }
